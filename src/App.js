3b441eabad471fbc3ba63048398834ed1c759917
vaibhav <vaibhavkharat.ex@gmail.com>
2023-02-08 09:55:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+let name = "KING"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque magni exercitationem blanditiis vel, recusandae aliquid porro qui nulla aperiam maiores sint animi delectus nisi quibusdam ad. Ipsam dicta distinctio tempore aliquid quod voluptate maxime.</p>
+      </div>
+    </>
   );
 }
 
